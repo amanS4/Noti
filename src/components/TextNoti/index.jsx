b8fb762e2e873a9dab30dbd70dc0f64294b92d97
amanSMylo <72736877+amanS4@users.noreply.github.com>
2022-01-24 01:13:12 +0530

    voice recognition

--- src/components/TextNoti/index.jsx
+++ src/components/TextNoti/index.jsx
@@ -3,6 +3,26 @@ import React, { useState } from "react";
 
 export default function TextNoti() {
   const [noteTitle, setNoteTitle] = useState("");
+  const [noteBody, setNoteBody] = useState("");
+  function startDictation() {
+    if (window.hasOwnProperty("webkitSpeechRecognition")) {
+      var recognition = new webkitSpeechRecognition();
+      recognition.continuous = false;
+      recognition.interimResults = false;
+      recognition.lang = "en-US";
+      recognition.start();
+
+      recognition.onresult = function (e) {
+        console.log(e.results[0][0].transcript);
+        setNoteBody((val) => val + ", " + e.results[0][0].transcript);
+        recognition.stop();
+      };
+
+      recognition.onerror = function (e) {
+        recognition.stop();
+      };
+    }
+  }
   return (
     <Stack
       display={"flex"}
@@ -21,7 +41,10 @@ export default function TextNoti() {
         variant="filled"
         placeholder="Notes"
         width={"90%"}
+        value={noteBody}
+        onChange={(e) => setNoteBody(e.target.value)}
       />
+      <div onClick={startDictation}>voice</div>
     </Stack>
   );
 }
